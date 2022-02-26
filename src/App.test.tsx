32a46ad248ffc63bc707b566c6ba04088a49ef5e
Igor Shainsky <igorshainsky@gmail.com>
2022-02-26 18:53:16 +0200

    Fix:Refactor test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders App component", async () => {
+    render(<App />);
+    expect(screen.queryByText(/Add Item/)).toBeNull();
+    expect(await screen.findByText(/Add Item/)).toBeInTheDocument();
+  });
 });
